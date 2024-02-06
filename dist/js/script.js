e790d6009a271fb5ce02b35157a7b03baf4b7f64
BagusSemesta2407 <semestabagus24@gmail.com>
2024-02-06 23:42:28 +0700

    banyak deh

--- dist/js/script.js
+++ dist/js/script.js
@@ -7,17 +7,32 @@ window.onscroll = function()
 
     if (window.scrollY > fixedNav) {
         header.classList.add('navbar-fixed');
+        updateMenuTextColor('text-white'); // Panggil fungsi untuk mengubah warna teks
     } else {
         header.classList.remove('navbar-fixed');
+        updateMenuTextColor('text-slate-900'); // Kembalikan warna teks ke nilai semula
     }
 }
 // hamburger
 
 const hamburger =document.querySelector('#hamburger');
 const navMenu=document.querySelector('#menu-nav');
+const logoText = document.querySelector('#logo-text');
 
 hamburger.addEventListener('click', function () {
     hamburger.classList.toggle('hamburger-active');
     navMenu.classList.toggle('hidden');
 })
 
+function updateMenuTextColor(colorClass) {
+    const menuItems = document.querySelectorAll('#menu-nav a');
+    menuItems.forEach(item => {
+        item.classList.remove('text-slate-900', 'text-white');
+        item.classList.add(colorClass);
+    });
+
+    // Tambahkan penanganan khusus untuk elemen logo
+    const logoText = document.querySelector('.px-4 a');
+    logoText.classList.remove('text-slate-900', 'text-white');
+    logoText.classList.add(colorClass);
+}
